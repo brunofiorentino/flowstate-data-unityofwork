e103c8dc92875687b731699a02690d69d13cfccf
Bruno Fiorentino <bruno.fiorentino@gmail.com>
2023-06-08 01:12:10 -0300

    DependencyInjectionScopeHelper initial implementation

---
+++ tests/Flowstate.Data.UnityOfWork.Tests.UnitTests/CodeSnippets/DependencyInjection/DependencyInjectionScopeHelper.cs
@@ -0,0 +1,267 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Flowstate.Data.UnityOfWork.Tests.UnitTests.CodeSnippets.DependencyInjection;
+
+public sealed class DependencyInjectionScopeHelper 
+{
+    // Worth it? Not sure. It's not compatible with .netstandard2.1 and it's still a service locator.
+    // Maybe it's better to let users manage scope by themselves for bg workers.
+
+    private readonly IServiceProvider _serviceProvider;
+
+    public DependencyInjectionScopeHelper(IServiceProvider serviceProvider) =>
+        _serviceProvider = serviceProvider;
+
+    public void ExecuteScoped<T1>(Action<T1> execute)
+        where T1 : class
+    {
+        using var scope = _serviceProvider.CreateScope();
+        execute(scope.ServiceProvider.GetRequiredService<T1>());
+    }
+
+    public async Task ExecuteScopedAsync<T1>(Func<T1, Task> execute)
+        where T1 : class
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        await execute(scope.ServiceProvider.GetRequiredService<T1>());
+    }
+
+
+    public void ExecuteScoped<T1, T2>(Action<T1, T2> execute)
+        where T1 : class
+        where T2 : class
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>());
+    }
+
+    public async Task ExecuteScopedAsync<T1, T2>(Func<T1, T2, Task> execute)
+        where T1 : class
+        where T2 : class
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        await execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>());
+    }
+
+
+    public void ExecuteScoped<T1, T2, T3>(Action<T1, T2, T3> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>());
+    }
+
+    public async Task ExecuteScopedAsync<T1, T2, T3>(Func<T1, T2, T3, Task> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        await execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>());
+    }
+
+
+    public void ExecuteScoped<T1, T2, T3, T4>(Action<T1, T2, T3, T4> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>());
+    }
+
+    public async Task ExecuteScopedAsync<T1, T2, T3, T4>(Func<T1, T2, T3, T4, Task> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        await execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>());
+    }
+
+
+
+    public void ExecuteScoped<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+        where T5 : class
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>(),
+            scope.ServiceProvider.GetRequiredService<T5>());
+    }
+
+    public async Task ExecuteScopedAsync<T1, T2, T3, T4, T5>(Func<T1, T2, T3, T4, T5, Task> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+        where T5 : class
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        await execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>(),
+            scope.ServiceProvider.GetRequiredService<T5>());
+    }
+
+
+
+    public void ExecuteScoped<T1, T2, T3, T4, T5, T6>(Action<T1, T2, T3, T4, T5, T6> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+        where T5 : class
+        where T6 : class
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>(),
+            scope.ServiceProvider.GetRequiredService<T5>(),
+            scope.ServiceProvider.GetRequiredService<T6>());
+    }
+
+    public async Task ExecuteScopedAsync<T1, T2, T3, T4, T5, T6>(Func<T1, T2, T3, T4, T5, T6, Task> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+        where T5 : class
+        where T6 : class
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        await execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>(),
+            scope.ServiceProvider.GetRequiredService<T5>(),
+            scope.ServiceProvider.GetRequiredService<T6>());
+    }
+
+
+    public void ExecuteScoped<T1, T2, T3, T4, T5, T6, T7>(Action<T1, T2, T3, T4, T5, T6, T7> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+        where T5 : class
+        where T6 : class
+        where T7 : class
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>(),
+            scope.ServiceProvider.GetRequiredService<T5>(),
+            scope.ServiceProvider.GetRequiredService<T6>(),
+            scope.ServiceProvider.GetRequiredService<T7>());
+    }
+
+    public async Task ExecuteScopedAsync<T1, T2, T3, T4, T5, T6, T7>(Func<T1, T2, T3, T4, T5, T6, T7, Task> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+        where T5 : class
+        where T6 : class
+        where T7 : class
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        await execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>(),
+            scope.ServiceProvider.GetRequiredService<T5>(),
+            scope.ServiceProvider.GetRequiredService<T6>(),
+            scope.ServiceProvider.GetRequiredService<T7>());
+    }
+
+    public void ExecuteScoped<T1, T2, T3, T4, T5, T6, T7, T8>(Action<T1, T2, T3, T4, T5, T6, T7, T8> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+        where T5 : class
+        where T6 : class
+        where T7 : class
+        where T8 : class
+    {
+        using var scope = _serviceProvider.CreateScope();
+
+        execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>(),
+            scope.ServiceProvider.GetRequiredService<T5>(),
+            scope.ServiceProvider.GetRequiredService<T6>(),
+            scope.ServiceProvider.GetRequiredService<T7>(),
+            scope.ServiceProvider.GetRequiredService<T8>());
+    }
+
+    public async Task ExecuteScopedAsync<T1, T2, T3, T4, T5, T6, T7, T8>(Func<T1, T2, T3, T4, T5, T6, T7, T8, Task> execute)
+        where T1 : class
+        where T2 : class
+        where T3 : class
+        where T4 : class
+        where T5 : class
+        where T6 : class
+        where T7 : class
+        where T8 : class
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        await execute(
+            scope.ServiceProvider.GetRequiredService<T1>(),
+            scope.ServiceProvider.GetRequiredService<T2>(),
+            scope.ServiceProvider.GetRequiredService<T3>(),
+            scope.ServiceProvider.GetRequiredService<T4>(),
+            scope.ServiceProvider.GetRequiredService<T5>(),
+            scope.ServiceProvider.GetRequiredService<T6>(),
+            scope.ServiceProvider.GetRequiredService<T7>(),
+            scope.ServiceProvider.GetRequiredService<T8>());
+    }
+}
---
+++ tests/Flowstate.Data.UnityOfWork.Tests.UnitTests/CodeSnippets/DependencyInjection/WhenCreatingDependencyInjectionScopes.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Flowstate.Data.UnityOfWork.Tests.UnitTests.CodeSnippets.DependencyInjection;
+
+public class WhenExecutingWithinDependencyInjectionScopes
+{
+    [Theory]
+    [MemberData(nameof(ServiceTypeInputRecords))]
+    public void CanResolveRequiredServicesViaExecuteScopedWithActionParameter(Type[] requiredServiceTypes)
+    {
+        var services = new ServiceCollection();
+
+        foreach (var serviceType in requiredServiceTypes)
+            services.AddScoped(serviceType);
+
+        using var serviceProvider = services.BuildServiceProvider();
+
+        var dependencyInjectionScopeHelper = new DependencyInjectionScopeHelper(serviceProvider);
+
+        var executeAction = ExecuteDelegates[requiredServiceTypes.Length - 1]!;
+        var executeActionGenericParameters = Enumerable
+            .Range(0, requiredServiceTypes.Length)
+            .Select(Type.MakeGenericMethodParameter)
+            .ToArray();
+
+        var executeScopedMethoInfo = dependencyInjectionScopeHelper
+            .GetType()
+            .GetMethod(
+                "ExecuteScoped",
+                requiredServiceTypes.Length,
+                BindingFlags.Public | BindingFlags.Instance,
+                null,
+                new Type[] { executeAction
+                    .GetType()
+                    .GetGenericTypeDefinition()
+                    .MakeGenericType(executeActionGenericParameters) },
+                null)!;
+
+        var genericExecuteMethodInfo = executeScopedMethoInfo.MakeGenericMethod(requiredServiceTypes);
+        genericExecuteMethodInfo.Invoke(dependencyInjectionScopeHelper, new[] { executeAction });
+    }
+
+    public static IEnumerable<object[]> ServiceTypeInputRecords = new object[][]
+    {
+        new object[]{ new Type[]{ typeof(Service1) } },
+        new object[]{ new Type[]{ typeof(Service1), typeof(Service2) } },
+        new object[]{ new Type[]{ typeof(Service1), typeof(Service2), typeof(Service3) } },
+        new object[]{ new Type[]{ typeof(Service1), typeof(Service2), typeof(Service3), typeof(Service4) } },
+        new object[]{ new Type[]{ typeof(Service1), typeof(Service2), typeof(Service3), typeof(Service4), typeof(Service5) } },
+        new object[]{ new Type[]{ typeof(Service1), typeof(Service2), typeof(Service3), typeof(Service4), typeof(Service5), typeof(Service6) } },
+        new object[]{ new Type[]{ typeof(Service1), typeof(Service2), typeof(Service3), typeof(Service4), typeof(Service5), typeof(Service6), typeof(Service7) } },
+        new object[]{ new Type[]{ typeof(Service1), typeof(Service2), typeof(Service3), typeof(Service4), typeof(Service5), typeof(Service6), typeof(Service7), typeof(Service8) } },
+    };
+
+    private record class Service1();
+    private record class Service2();
+    private record class Service3();
+    private record class Service4();
+    private record class Service5();
+    private record class Service6();
+    private record class Service7();
+    private record class Service8();
+
+    private static void Execute1(Service1 s1) { }
+    private static void Execute2(Service1 s1, Service2 s2) { }
+    private static void Execute3(Service1 s1, Service2 s2, Service3 s3) { }
+    private static void Execute4(Service1 s1, Service2 s2, Service3 s3, Service4 s4) { }
+    private static void Execute5(Service1 s1, Service2 s2, Service3 s3, Service4 s4, Service5 s5) { }
+    private static void Execute6(Service1 s1, Service2 s2, Service3 s3, Service4 s4, Service5 s5, Service6 s6) { }
+    private static void Execute7(Service1 s1, Service2 s2, Service3 s3, Service4 s4, Service5 s5, Service6 s6, Service7 s7) { }
+    private static void Execute8(Service1 s1, Service2 s2, Service3 s3, Service4 s4, Service5 s5, Service6 s6, Service7 s7, Service8 s8) { }
+
+    private static Delegate[] ExecuteDelegates = Enumerable
+        .Range(1, 8)
+        .Select(n =>
+            typeof(WhenExecutingWithinDependencyInjectionScopes)
+                .GetMethod($"Execute{n}", BindingFlags.NonPublic | BindingFlags.Static)!)
+        .Select(CreateActionDelegate)
+        .ToArray();
+
+    private static Delegate CreateActionDelegate(MethodInfo methodInfo) =>
+        Delegate.CreateDelegate(
+            Expression.GetActionType(methodInfo.GetParameters().Select(p => p.ParameterType).ToArray()),
+            methodInfo);
+}
